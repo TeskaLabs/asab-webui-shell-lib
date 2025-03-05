d5c62735b11198c135606f59e001349e713f94b1
akonshina <arina.konshina@teskalabs.com>
2025-03-05 08:44:35 +0100

    .startsWith('application/json;')

--- src/containers/Application.js
+++ src/containers/Application.js
@@ -323,7 +323,7 @@ class Application extends Component {
 			that.popNetworkingIndicator();
 
 			// Check if the response content type is 'application/json' or starts with 'application/json'
-			if ((response.headers['content-type'] == 'application/json') || (response.headers['content-type']?.startsWith('application/json'))) {
+			if ((response.headers['content-type'] == 'application/json') || (response.headers['content-type']?.startsWith('application/json;'))) {
 				// If the response is JSON, then parse it with respect to BigInt
 				// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 				try {
