1a274f1d3568d081f4fe23fc7d681aa9578d7efb
Pe5h4 <pesekf@email.cz>
2025-08-28 11:31:21 +0200

    Add todo

--- src/modules/attentionrequired/service.jsx
+++ src/modules/attentionrequired/service.jsx
@@ -100,6 +100,7 @@ export default class AttentionRequiredService extends Service {
 	// Method for distributing the data into Application store
 	distributeData(data) {
 		const transformedData = this.transformData(data);
+		// TODO: use a pubsub instead of the appstore dispatch
 		this.App?.AppStore?.dispatch?.({ type: SET_ATTENTION_REQUIRED_BEACON, beacon: transformedData });
 	}
 
