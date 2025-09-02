f626a06b29c635ef7b4057148aee350aef098e38
Pe5h4 <pesekf@email.cz>
2025-09-02 17:10:52 +0200

    Upgrade to react router v7

--- src/containers/Sidebar/NavbarBrand.js
+++ src/containers/Sidebar/NavbarBrand.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 
 import { getBrandImage } from '../../components/branding/BrandImage';
 import { useAppSelector } from 'asab_webui_components';
