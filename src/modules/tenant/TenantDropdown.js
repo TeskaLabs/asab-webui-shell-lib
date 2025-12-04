57c26afc5ee204a4054d1c895c0d906c015be31d
JanMajer86 <majer.jan.1986@gmail.com>
2025-12-04 11:31:12 +0100

    refactor tenant dropdown - first iteration

--- src/modules/tenant/TenantDropdown.js
+++ src/modules/tenant/TenantDropdown.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useAppSelector } from 'asab_webui_components';
+import { ButtonWithAuthz } from 'asab_webui_components/seacat-auth';
 import { useTranslation } from 'react-i18next';
 
 import {
 	UncontrolledDropdown,
 	DropdownItem,
-	DropdownMenu, 
+	DropdownMenu,
 	DropdownToggle,
 } from 'reactstrap';
 
@@ -15,13 +16,19 @@ export default function TenantDropdown() {
 	const current = useAppSelector(state => state?.tenant?.current);
 	const tenants = useAppSelector(state => state?.tenant?.tenants);
 
+	const resources = useAppSelector(state => state?.auth?.resources);
+	const tenantCreateResource = 'authz:superuser'; // "seacat:tenant:create";
+
+	// Check if user can create tenant (same logic as ButtonWithAuthz uses internally)
+	const canCreateTenant = resources?.includes(tenantCreateResource) || resources?.includes('authz:superuser');
+
 	return (
 		<UncontrolledDropdown direction="down" title={t('tenant|Tenant')}>
 			<DropdownToggle nav caret>
 				<i className="bi bi-house-lock pe-2"></i>
 				<TenantLabel tenant={current}/>
 			</DropdownToggle>
-			{ (tenants && tenants.length > 0) ?
+			{(tenants && tenants.length > 0 || canCreateTenant) && (
 				<DropdownMenu className="shadow">
 					<DropdownItem header>Tenants</DropdownItem>
 					{tenants.map((tenant, i) => (
@@ -29,12 +36,29 @@ export default function TenantDropdown() {
 							<TenantLabel tenant={tenant}/>
 						</DropdownItem>
 					))}
+					{canCreateTenant && (
+						<>
+							<DropdownItem divider />
+							<DropdownItem header>
+								<ButtonWithAuthz
+									type='button'
+									outline
+									color='primary'
+									onClick={() => confirmForm('/config/tenant/!create')}
+									resource={tenantCreateResource}
+									resources={resources}
+									hideOnUnauthorizedAccess={true}
+								>
+									{t('TenantDetailScreen|Create tenant')}
+								</ButtonWithAuthz>
+							</DropdownItem>
+						</>
+					)}
 				</DropdownMenu>
-			 : null}
+			)}
 		</UncontrolledDropdown>
 	);
 }
-
 
 function TenantLabel({tenant}) {
 	return (
