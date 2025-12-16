c2a84bddbf98f57edf52884096f841ec6fdc6990
Pe5h4 <pesekf@email.cz>
2025-12-16 12:11:16 +0100

    Make Invite other users a Header Dropdown module

--- src/index.js
+++ src/index.js
@@ -7,3 +7,4 @@ export { default as TenantModule } from './modules/tenant';
 export { default as AuthModule } from './modules/auth';
 export { default as AboutModule } from './modules/about';
 export { default as AttentionRequiredModule } from './modules/attentionrequired/index.jsx';
+export { default as InviteModule } from './modules/invite';
