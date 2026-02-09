9a7eed484785d647afbf836e9724a5c0abf5a040
JanMajer86 <majer.jan.1986@gmail.com>
2026-02-09 16:30:30 +0100

    conditioning refactor

--- src/modules/tenant/TenantDropdown.js
+++ src/modules/tenant/TenantDropdown.js
@@ -17,36 +17,39 @@ export default function TenantDropdown({ app }) {
 	const current = useAppSelector(state => state?.tenant?.current);
 	const tenants = useAppSelector(state => state?.tenant?.tenants);
 	const canCreateTenant = isAuthorized(['lmio:tenant:create'], app);
-
 	const tenantsAvailable = tenants && tenants.length > 0;
+	const showDropdown = tenantsAvailable || canCreateTenant;
 
 	return (
 		<UncontrolledDropdown direction="down" title={t('tenant|Tenant')}>
 			<DropdownToggle nav caret>
 				<i className="bi bi-house-lock pe-2"></i>
-				<TenantLabel tenant={current}/>
+				<TenantLabel tenant={current} />
 			</DropdownToggle>
-			<DropdownMenu className="shadow">
-				{tenantsAvailable && (
-					<>
-						<DropdownItem header>{t('TenantDropdown|Tenants')}</DropdownItem>
-						{tenants?.map((tenant, i) => (
-							<DropdownItem key={i} tag="a" href={'?tenant='+tenant+'#/'}>
-								<TenantLabel tenant={tenant}/>
+			{showDropdown && (
+				<DropdownMenu className="shadow">
+					{tenantsAvailable && (
+						<>
+							<DropdownItem header>
+								{t('TenantDropdown|Tenants')}
 							</DropdownItem>
-						))}
-					</>
-				)}
-				{/* Link to lmio_trex_webui tenant creation screen */}
-				{canCreateTenant && (
-					<>
-						<DropdownItem divider />
-						<DropdownItem tag={Link} to="/config/tenant/!create">
-							{t('TenantDropdown|Create tenant')}
-						</DropdownItem>
-					</>
-				)}
-			</DropdownMenu>
+							{tenants?.map((tenant, i) => (
+								<DropdownItem key={i} tag="a" href={`?tenant=${tenant}#/`}>
+									<TenantLabel tenant={tenant} />
+								</DropdownItem>
+							))}
+						</>
+					)}
+					{canCreateTenant && (
+						<>
+							{tenantsAvailable && <DropdownItem divider />}
+							<DropdownItem tag={Link} to="/config/tenant/!create">
+								{t('TenantDropdown|Create tenant')}
+							</DropdownItem>
+						</>
+					)}
+				</DropdownMenu>
+			)}
 		</UncontrolledDropdown>
 	);
 }
