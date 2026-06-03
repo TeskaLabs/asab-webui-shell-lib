2f3f7a563534e0dcea4febc87de084fe67632b21
Ales Teska <ales.teska@teskalabs.com>
2026-06-03 23:59:14 +0200

    Fix

--- src/modules/attentionrequired/service.jsx
+++ src/modules/attentionrequired/service.jsx
@@ -31,6 +31,8 @@ export default class AttentionRequiredService extends Service {
 			return;
 		}
 
+		return;  // Disabled for development purposes
+
 		const BeaconServiceClient = this.App.createWebSocket('asab-remote-control', `/beacon/${currentTenant}/ws`);
 		this.beaconWebSocket = BeaconServiceClient;
 
