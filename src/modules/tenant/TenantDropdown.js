0b019376f85bc487531f0a4223637570b50407f6
Pe5h4 <pesekf@email.cz>
2026-07-08 17:25:38 +0200

    Functionally refactor a tenant dropdown

--- src/modules/tenant/TenantDropdown.js
+++ src/modules/tenant/TenantDropdown.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { useAppSelector } from 'asab_webui_components';
 import { useTranslation } from 'react-i18next';
 
 import {
-	UncontrolledDropdown,
+	Dropdown,
 	DropdownItem,
-	DropdownMenu, 
+	DropdownMenu,
 	DropdownToggle,
 } from 'reactstrap';
 
 
 export default function TenantDropdown() {
 	const { t } = useTranslation();
+	const [isOpen, setIsOpen] = useState(false);
 	const current = useAppSelector(state => state?.tenant?.current);
 	const tenants = useAppSelector(state => state?.tenant?.tenants);
 
+	const toggle = () => setIsOpen(prev => !prev);
+
 	return (
-		<UncontrolledDropdown direction="down" title={t('tenant|Tenant')}>
+		<Dropdown isOpen={isOpen} toggle={toggle} direction="down" title={t('General|Tenant')}>
 			<DropdownToggle nav caret>
 				<i className="bi bi-house-lock pe-2"></i>
 				<TenantLabel tenant={current}/>
 			</DropdownToggle>
-			{ (tenants && tenants.length > 0) ?
-				<DropdownMenu className="shadow">
-					<DropdownItem header>Tenants</DropdownItem>
+			{isOpen && tenants?.length > 0 && (
+				<DropdownMenu className="shadow overflow-y-auto" style={{ maxHeight: '20em' }}>
+					<DropdownItem header>{t('General|Tenants')}</DropdownItem>
 					{tenants.map((tenant, i) => (
 						<DropdownItem key={i} tag="a" href={'?tenant='+tenant+'#/'}>
 							<TenantLabel tenant={tenant}/>
 						</DropdownItem>
 					))}
 				</DropdownMenu>
-			 : null}
-		</UncontrolledDropdown>
+			)}
+		</Dropdown>
 	);
 }
 
